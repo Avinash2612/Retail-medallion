b77a564a11181fe2741005c155dd20510a742434
AVINASH V <67922562+Avinash2612@users.noreply.github.com>
2025-12-02 18:55:02 +0530

    Update bronze_to_silver.py

--- bronze_to_silver.py
+++ bronze_to_silver.py
@@ -6,30 +6,15 @@ spark = SparkSession.builder \
     .master("local[*]") \
     .getOrCreate()
 
-# Read from bronze
 df = spark.read.parquet("bronze/retail_bronze.parquet")
-
-# 1. Rename columns (remove spaces, lowercase)
 df = df.toDF(*[c.replace(" ", "_").lower() for c in df.columns])
-
-# 2. Convert date string → DateType
 df = df.withColumn("date", to_date(col("date"), "dd-MM-yyyy"))
-
-
-# 3. Trim and format text fields
 df = df.withColumn("gender", trim(col("gender"))) \
        .withColumn("product_category", trim(col("product_category")))
-
-# 4. Remove duplicates (based on transaction_id)
 df = df.dropDuplicates(["transaction_id"])
-
-# 5. Recalculate total_amount for consistency
 df = df.withColumn("total_amount", col("quantity") * col("price_per_unit"))
-
-# 6. Drop rows with nulls in critical columns
 df = df.dropna(subset=["transaction_id", "date", "quantity"])
 
-# Write to silver
 df.write.mode("overwrite").parquet("silver/retail_silver.parquet")
 
 print("Silver layer written successfully!")
